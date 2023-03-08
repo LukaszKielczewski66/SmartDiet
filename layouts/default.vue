c82656b6ed684cbcebea6ac054a9caabdcffc9d9
Michał Jaroń <64842623+streetmarker@users.noreply.github.com>
2023-03-08 20:47:25 +0100

    base routing

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,22 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title
+        ><v-btn to="/">{{ title }}</v-btn></v-toolbar-title
+      >
+      <v-spacer />
+      <v-btn :to="login.to"
+        ><v-icon>{{ login.icon }}</v-icon
+        >{{ login.title }}</v-btn
+      >
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-navigation-drawer v-model="drawer" left temporary fixed>
       <v-list>
         <v-list-item
           v-for="(item, i) in items"
@@ -21,38 +31,6 @@
           <v-list-item-content>
             <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title>{{ title }}</v-toolbar-title>
-      <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
-    </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
@@ -72,20 +50,24 @@ export default {
       fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
+          icon: 'mdi-chef-hat',
+          title: 'Recipes',
           to: '/',
         },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
+        // {
+        //   icon: 'mdi-chart-bubble',
+        //   title: 'Inspire',
+        //   to: '/inspire',
+        // },
       ],
+      login: {
+        icon: 'mdi-account-circle-outline',
+        title: 'Login',
+        to: 'login',
+      },
       miniVariant: false,
       right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
+      title: 'Smart Diet',
     }
   },
 }
---
+++ pages/register.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="6" md="4">
+      <v-card class="logo py-4 d-flex justify-center">
+        <v-form @submit.prevent>
+          <h1>Register</h1>
+          <v-text-field
+            v-model="firstName"
+            :rules="rules"
+            label="First name"
+          ></v-text-field>
+          <v-btn type="submit" block class="mt-2">Submit</v-btn>
+          <br />
+          <v-btn color="primary" to="/login">I have account</v-btn>
+        </v-form>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data: () => ({
+    firstName: '',
+    rules: [
+      (value) => {
+        if (value) return true
+
+        return 'You must enter a first name.'
+      },
+    ],
+  }),
+  head() {
+    // SEO
+    return {
+      title: 'register',
+    }
+  },
+}
+</script>
+<style></style>
