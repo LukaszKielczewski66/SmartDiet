b0e88d8d8c51f7c4658d0df8e304e3da2dd9a175
LukaszKielczewski66 <LukaszKielczewski66@gmail.com>
2023-03-09 18:33:59 +0100

    add register form

--- pages/register.vue
+++ pages/register.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-row justify="center" align="center">
+  <!-- <v-row justify="center" align="center">
     <v-col cols="12" sm="6" md="4">
       <v-card class="logo py-4 d-flex justify-center">
         <v-form @submit.prevent>
@@ -15,6 +15,100 @@
         </v-form>
       </v-card>
     </v-col>
+  </v-row> -->
+  <v-row class="d-flex justify-center align-center">
+    <v-card class="register-card mx-8 mt-10">
+      <v-card-title>{{ currentTitle }}</v-card-title>
+      <v-window v-model="step">
+        <v-window-item :value="1">
+          <v-card-text>
+            <v-row>
+              <v-col cols="6">
+                <v-text-field v-model="firstname" label="Imię"></v-text-field>
+              </v-col>
+              <v-col cols="6">
+                <v-text-field
+                  v-model="lastname"
+                  label="Nazwisko"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-text-field v-model="email" label="E-mail"></v-text-field>
+            <v-text-field v-model="password" label="Hasło"></v-text-field>
+            <v-checkbox v-model="terms" label="Akceptuje warunki korzystania"></v-checkbox>
+          </v-card-text>
+        </v-window-item>
+        <v-window-item :value="2">
+          <v-card-text>
+            <v-row>
+              <v-col cols="12" md="6">
+                <v-text-field
+                  v-model="height"
+                  label="Wzrost"
+                  type="number"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" md="6">
+                <v-text-field
+                  v-model="weight"
+                  label="Waga"
+                  type="number"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" md="6">
+                <v-text-field
+                  v-model="age"
+                  label="Wiek"
+                  type="number"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" md="6">
+                <v-select
+                  v-model="gender"
+                  :items="genders"
+                  label="Płeć"
+                  clearable
+                ></v-select>
+              </v-col>
+              <v-col cols="12">
+              <v-select
+                  v-model="activity"
+                  :items="activities"
+                  label="Aktywność fizyczna"
+                  clearable
+                ></v-select>
+              </v-col>
+              <v-col cols="12">
+                <v-select
+                  v-model="goal"
+                  :items="goals"
+                  label="Twój cel"
+                  clearable
+                ></v-select>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-window-item>
+        <v-window-item :value="3">
+        </v-window-item>
+      </v-window>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn v-if="step === 1" color="primary" tile @click="step++">
+          Przejdź dalej
+        </v-btn>
+        <v-btn v-if="step === 2" color="primary" tile @click="step--">
+          Powrót
+        </v-btn>
+        <v-btn v-if="step === 2" color="primary" tile @click="createAccount()">
+          Utwórz konto!
+        </v-btn>
+        <v-btn v-if="step === 3" color="primary" tile>
+         Zaloguj się
+        </v-btn>
+        <v-spacer v-if="step === 3"></v-spacer>
+      </v-card-actions>
+    </v-card>
   </v-row>
 </template>
 
@@ -22,14 +116,27 @@
 export default {
   name: 'IndexPage',
   data: () => ({
-    firstName: '',
-    rules: [
-      (value) => {
-        if (value) return true
-
-        return 'You must enter a first name.'
-      },
-    ],
+    step: 1,
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: '',
+    terms: false,
+    height: '',
+    weight: '',
+    age: '',
+    gender: '',
+    genders: ['Mężczyzna', 'Kobieta', 'Helikopter Bojowy'],
+    activity: '',
+    activities: ['Mała', 'Średnia', 'Duża'],
+    goal: '',
+    goals: ['Chcę schudnąć', 'Chcę utrzymać wagę', 'Chcę przytyć'],
+    // rules: [
+    //   (value) => {
+    //     if (value) return true
+    //     return 'You must enter a first name.'
+    //   },
+    // ],
   }),
   head() {
     // SEO
@@ -37,6 +144,37 @@ export default {
       title: 'register',
     }
   },
+  computed: {
+    currentTitle() {
+      switch (this.step) {
+        case 1:
+          return 'Zarejestruj się'
+        case 2:
+          return 'Podaj swoje dane'
+        default:
+          return 'Konto zostało stworzone!'
+      }
+    },
+  },
+  methods: {
+    createAccount () {
+      // walidacja
+      this.step++;
+      this.$axios
+      .post()
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+    }
+  }
 }
 </script>
-<style></style>
+
+<style>
+.register-card {
+  max-width: 500px;
+}
+</style>
