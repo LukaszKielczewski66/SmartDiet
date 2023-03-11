dbb7f9216ad897881566f6bb1fe6365dc516ca50
Michał Jaroń <64842623+streetmarker@users.noreply.github.com>
2023-03-11 19:55:33 +0100

    api update

--- layouts/default.vue
+++ layouts/default.vue
@@ -72,3 +72,8 @@ export default {
   },
 }
 </script>
+<style>
+* {
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+</style>
--- pages/login.vue
+++ pages/login.vue
@@ -5,20 +5,57 @@
         <v-form @submit.prevent>
           <h1>Login</h1>
           <v-text-field
-            v-model="firstName"
+            v-model="email"
             :rules="rules"
-            label="First name"
+            label="Email"
+          ></v-text-field>
+          <v-text-field
+            v-model="password"
+            :rules="rules"
+            label="Password"
           ></v-text-field>
           <v-btn type="submit" block class="mt-2">Submit</v-btn>
           <br />
           <v-btn color="primary" to="/register">I don't have an account</v-btn>
+          <v-alert v-if="successMsg" type="success">{{ successMsg }}</v-alert>
           <v-btn type="submit" block class="mt-2" @click="login">1. test login</v-btn>
           <v-btn type="submit" block class="mt-2" @click="getRecipes">2. test get recipes</v-btn>
+          <v-btn type="submit" block class="mt-2" @click="logout">2. test logout</v-btn>
+          <v-alert v-if="errorMsg" type="error">{{ errorMsg }}</v-alert>
         </v-form>
       </v-card>
       <v-card>
         <v-card-title>TEST DATA</v-card-title>
-        {{ recipes }}
+        <v-item-group selected-class="bg-primary">
+          <v-container>
+            <v-row>
+              <v-col
+                v-for="recipe in recipes"
+                :key="recipe.recipe_id"
+                cols="12"
+                md="4"
+              >
+                <v-item v-slot="{ isSelected, selectedClass, toggle }">
+                  <v-card
+                    :class="['d-flex align-center', selectedClass]"
+                    dark
+                    height="200"
+                    @click="toggle"
+                  >
+                    <div
+                      class="text-h5 flex-grow-1 text-center"
+                    >
+                    {{ recipe.recipe_name }}
+                    {{ isSelected }}
+                    <v-dialog>{{ recipe.recipe_name }}</v-dialog>
+                  </div>
+                </v-card>
+              </v-item>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-item-group>
+      {{ recipes }}
       </v-card>
     </v-col>
   </v-row>
@@ -28,16 +65,19 @@
 export default {
   name: 'IndexPage',
   data: () => ({
-    firstName: '',
+    email: 'john.doe@example.com', // test
+    password: 'password', // test
     rules: [
       (value) => {
         if (value) return true
 
-        return 'You must enter a first name.'
+        return 'Required'
       },
     ],
     token:'',
     recipes:[],
+    errorMsg:'',
+    successMsg: '',
   }),
   head() {
     // SEO
@@ -46,20 +86,24 @@ export default {
     }
   },
   methods: {
-    async login () {
+    async login () { // przenieść do store
+      this.errorMsg = ''
       const userData = {
-        email: "john.doe@example.com",
-        password: "password"
+        email: this.email,
+        password: this.password
       };
       try {
         const response = await this.$axios.post('/api/login', userData);
         this.token = response.data.token;
+        response.status ? this.successMsg = 'Logged in' : this.errorMsg = response.statusTex;
       } catch (err) {
+        err.response.data.message ? this.errorMsg = err.response.data.message : this.errorMsg = err.response.statusTex;
         // eslint-disable-next-line no-console
         console.log(err)
       }
     },
     async getRecipes () {
+      this.errorMsg = ''
       try {
         const response = await this.$axios.get('/api/recipes',{
           // body
@@ -70,11 +114,14 @@ export default {
         });
         this.recipes = response.data;
       } catch (err) {
-        // eslint-disable-next-line no-console
-        console.log(this.token)
+        this.errorMsg = err.response.data.message
         // eslint-disable-next-line no-console
         console.log(err)
       }
+    },
+    logout() { // przenieść do store
+      this.token = ''
+      this.successMsg = ''
     }
   }
 }
